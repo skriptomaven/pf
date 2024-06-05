44b4b12f20af8413a72a39a1c4742b4574a6b532
skriptomaven <bygeoffreyouma@gmail.com>
2024-06-06 01:09:59 +0300

    Auth implemented!

--- pf/models.py
+++ pf/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=30)
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=20)
+
+class Video(models.Model):
+    title = models.CharField(max_length=47)
+    description = models.CharField(max_length=100)
+    author = models.CharField(max_length=30)
+    date = models.DateField('date uploaded')
+    comment = models.CharField(max_length=100)
--- pikaflix/urls.py
+++ pikaflix/urls.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from django.urls import path
 
 
-from pf.views import home_view, login_view, signup_view
+from pf.views import home, login, signup
 
 
 urlpatterns = [
-    path('', home_view, name='home_view'),
-    path('login/', login_view, name='login_view'),
-    path('signup/', signup_view, name='signup_view'),
+    path('', home, name='home'),
+    path('login/', login, name='login'),
+    path('signup/', signup, name='signup'),
     path('admin/', admin.site.urls),
 ]
