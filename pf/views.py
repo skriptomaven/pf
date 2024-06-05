5f69b5a0b9305b6a5554af786cb288c78c2faa2f
skriptomaven <bygeoffreyouma@gmail.com>
2024-06-05 03:54:31 +0300

    Add static files

--- pf/views.py
+++ pf/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home_view(request):
+    '''
+    This is the 1st page users will see.
+    '''
+    return render(request, 'pf/home.html', {})
+
+def login_view(request):
+    '''
+    Users will use this view to login.
+    '''
+    return render(request, 'pf/login.html', {})
+
+def signup_view(request):
+    '''
+    Users will use this view to sign-up.
+    '''
+    return render(request, 'pf/signup.html', {})
