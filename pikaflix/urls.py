d0422d6847f67e3a54f427ffddeaf33695f5ffd7
skriptomaven <bygeoffreyouma@gmail.com>
2024-06-06 09:19:40 +0300

    Update views(templates)

--- pikaflix/urls.py
+++ pikaflix/urls.py
@@ -2,12 +2,14 @@ from django.contrib import admin
 from django.urls import path
 
 
-from pf.views import home, login, signup
+from pf.views import home, login, signup, logout, video
 
 
 urlpatterns = [
     path('', home, name='home'),
     path('login/', login, name='login'),
     path('signup/', signup, name='signup'),
+    path('logout/', logout, name='logout'),
+    path('video/', video, name='video'),
     path('admin/', admin.site.urls),
 ]
