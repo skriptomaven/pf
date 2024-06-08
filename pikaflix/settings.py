6bb5b6fc18a4b0da0e33addbebdf31c696fcf94a
skriptomaven <bygeoffreyouma@gmail.com>
2024-06-08 23:47:24 +0300

    Uploaded videos now play

--- pikaflix/settings.py
+++ pikaflix/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -110,6 +111,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- pikaflix/urls.py
+++ pikaflix/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 from pf.views import home, login, signup, logout, upload_video
@@ -12,4 +14,4 @@ urlpatterns = [
     path('logout/', logout, name='logout'),
     path('upload_video/', upload_video, name='upload_video'),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
